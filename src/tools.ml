629ef96a7cdc9c9af9b9eb01791fc48cc1a73bf6
guillaume <gche@insa-toulouse.fr>
2025-11-21 15:16:24 +0100

    Implémentation de la fonction gmap du module Tools

--- src/tools.ml
+++ src/tools.ml
@@ -4,6 +4,6 @@ open Graph
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
 let clone_nodes (gr:'a graph) = n_fold gr (fun gra i -> new_node gra i) empty_graph
 
-let gmap _gr _f = assert false
-(* Replace _gr and _f by gr and f when you start writing the real function. *)
+let gmap gr f = e_fold gr (fun gr1 arc1 -> let arc2 = {arc1 with lbl = (f arc1.lbl)} in new_arc gr1 arc2) (clone_nodes gr)
+
 let add_arc _gr _id1 _id2 _n = assert false
