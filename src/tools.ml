54577c09451136454db6f1116a09333b68c77d39
guillaume <gche@insa-toulouse.fr>
2025-11-21 15:15:43 +0100

    Implémentation de la fonction clone_nodes du module Tools

--- src/tools.ml
+++ src/tools.ml
@@ -2,7 +2,8 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes _gr = assert false
+let clone_nodes (gr:'a graph) = n_fold gr (fun gra i -> new_node gra i) empty_graph
+
 let gmap _gr _f = assert false
 (* Replace _gr and _f by gr and f when you start writing the real function. *)
 let add_arc _gr _id1 _id2 _n = assert false
